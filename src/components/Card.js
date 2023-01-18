faf5225cb8cdbc665d96aa2a094d2f8ce7154e24
Keyur Raval <ravalkeyur054@gmail.com>
2023-01-18 19:44:00 +0530

    Search Bar added

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,8 +4,8 @@ const Card = () => {
     return (
         <div>
             <div>
-                <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
-                    <img src="..." className="card-img-top" alt="..." />
+                <div className="card m-4" style={{ "width": "18rem", "maxHeight": "360px" }}>
+                    <img src="" className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">Some quick example text to build on the card.</p>
