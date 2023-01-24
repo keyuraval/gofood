3ea4096a905e7032dcccd4e7e887d9b03879fb51
Keyur Raval <ravalkeyur054@gmail.com>
2023-01-24 20:27:49 +0530

    db connection refactored

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,21 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://keyur:shivam6503@cluster0.bxsbf1r.mongodb.net/?retryWrites=true&w=majority';
+const mongoURI = 'mongodb+srv://keyur:shivam6503@cluster0.bxsbf1r.mongodb.net/gofood?retryWrites=true&w=majority';
 mongoose.set('strictQuery', true);
 
 const mongoDB = async () => {
-    await mongoose.connect(mongoURI, { useNewUrlParser: true }, (err, result) => {
+    mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
         if (err) {
             console.log(err);
         } else {
             console.log("Connected");
-
+            const fetched_data = await mongoose.connection.db.collection("food_items");
+            fetched_data.find({}).toArray(function (err, data) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log();
+                }
+            });
         }
     });
 }
