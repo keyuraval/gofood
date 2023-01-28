7dd5b6b64e86cd695d6f3a423cd26cdae24da128
Keyur Raval <ravalkeyur054@gmail.com>
2023-01-28 18:55:29 +0530

    JWT Authntication added

--- backend/Routes/CreatUser.js
+++ backend/Routes/CreatUser.js
@@ -3,10 +3,13 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const jwtSecret = "Mynameisblahblahblahblahiamfromblahland"
 
+//SIGNUP
 router.post("/createuser",
     body('email').isEmail(),
-    // password must be at least 5 chars long
     body('name').isLength({ min: 5 }),
     body('password', 'Incorrect Password').isLength({ min: 5 }),
     async (req, res) => {
@@ -15,12 +18,15 @@ router.post("/createuser",
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
+
+        const salt = await bcrypt.genSalt(10);
+        let secPassword = await bcrypt.hash(req.body.password, salt);
         try {
             await User.create({
-                name: "Keyur",
-                password: "abc123",
-                email: "abc@gmail.com",
-                location: "Mehsana"
+                name: req.body.name,
+                password: secPassword,
+                email: req.body.email,
+                location: req.body.location
 
             })
             res.json({ success: true });
@@ -30,6 +36,44 @@ router.post("/createuser",
             res.json({ success: false });
 
 
+        }
+
+    })
+
+
+//LOGIN
+router.post("/loginuser",
+    body('email').isEmail(),
+    body('password', 'Incorrect Password').isLength({ min: 5 }),
+    async (req, res) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        let email = req.body.email;
+        try {
+            let userData = await User.findOne({ email });
+            if (!userData) {
+                return res.status(400).json({ errors: "Try login with correct credentials." });
+            }
+            const pwdCompare = await bcrypt.compare(req.body.password, userData.password)
+            if (!pwdCompare) {
+                return res.status(400).json({ errors: "Try login with correct credentials." });
+            }
+            const data = {
+                user: {
+                    id: userData.id
+                }
+            }
+            const authToken = jwt.sign(data, jwtSecret)
+
+            return res.json({ success: true, authToken: authToken })
+
+        } catch (error) {
+            console.log(error);
+            res.json({ success: false });
+            return error;
         }
 
     })
