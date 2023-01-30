8210d2838980d815b04d5dba3b6e74ed50af3819
Keyur Raval <ravalkeyur054@gmail.com>
2023-01-30 23:06:04 +0530

    Data fetched from MongoDB

--- backend/db.js
+++ backend/db.js
@@ -10,12 +10,22 @@ const mongoDB = async () => {
         } else {
             console.log("Connected");
             const fetched_data = await mongoose.connection.db.collection("food_items");
-            fetched_data.find({}).toArray(function (err, data) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log();
-                }
+            fetched_data.find({}).toArray(async function (err, data) {
+                const foodCatrgory = await mongoose.connection.db.collection("foodCategory");
+                foodCatrgory.find({}).toArray(function (err, catData) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        global.food_items = data;
+                        global.foodCategory = catData;
+                    }
+                })
+
+                // if (err) {
+                //     console.log(err);
+                // } else {
+                //     global.food_items = data;
+                // }
             });
         }
     });
