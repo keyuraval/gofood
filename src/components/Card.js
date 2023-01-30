ef82bbf3c8d62bbba3f774d0d18abe35bc60ada8
Keyur Raval <ravalkeyur054@gmail.com>
2023-01-30 23:08:23 +0530

    Dynamically data displayed on card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
+
+    let options = props.options;
+    let priceOptions = Object.keys(options);
     return (
         <div>
             <div>
                 <div className="card m-4" style={{ "width": "18rem", "maxHeight": "360px" }}>
-                    <img src="" className="card-img-top" alt="..." />
+                    <img src={props.imgSrc} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card.</p>
+                        <h5 className="card-title">{props.foodName}</h5>
                         <div className='container w-100'>
                             <select className='m-2 h-100 bg-success rounded'>
                                 {Array.from(Array(6), (e, i) => {
@@ -20,9 +22,11 @@ const Card = () => {
                                 })}
                             </select>
                             <select className='m-2 h-100  bg-success rounded'>
-                                <option value="half">Half</option>
-                                <option value="full">Full</option>
+                                {priceOptions.map((data) => {
+                                    return <option key={data} value={data}>{data}</option>
+                                })}
                             </select>
+
                             <div className='d-inline h-100 fs-5'>Total Price</div>
                         </div>
                     </div>
