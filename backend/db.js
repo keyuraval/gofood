da1e5f8e095b36b3b5d0f235060f1b7529b1da0b
Keyur Raval <ravalkeyur054@gmail.com>
2023-02-06 18:45:13 +0530

    db connection refactored

--- backend/db.js
+++ backend/db.js
@@ -3,31 +3,25 @@ const mongoose = require('mongoose');
 const mongoURI = 'mongodb://keyur:shivam6503@ac-9owmljc-shard-00-00.bxsbf1r.mongodb.net:27017,ac-9owmljc-shard-00-01.bxsbf1r.mongodb.net:27017,ac-9owmljc-shard-00-02.bxsbf1r.mongodb.net:27017/gofood?ssl=true&replicaSet=atlas-zyjz8g-shard-0&authSource=admin&retryWrites=true&w=majority'
 mongoose.set('strictQuery', true);
 
-const mongoDB = async () => {
+module.exports = function (callback) {
     mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Connected");
-            const fetched_data = await mongoose.connection.db.collection("food_items");
-            fetched_data.find({}).toArray(async function (err, data) {
-                const foodCatrgory = await mongoose.connection.db.collection("foodCategory");
-                foodCatrgory.find({}).toArray(function (err, catData) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        global.food_items = data;
-                        global.foodCategory = catData;
-                    }
-                })
+        // mongoDbClient.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
+        if (err) console.log("---" + err)
+        else {
+            // var database =
+            console.log("connected to mongo")
+            const foodCollection = await mongoose.connection.db.collection("food_items");
+            foodCollection.find({}).toArray(async function (err, data) {
+                const categoryCollection = await mongoose.connection.db.collection("foodCategory");
+                categoryCollection.find({}).toArray(async function (err, Catdata) {
+                    callback(err, data, Catdata);
 
-                // if (err) {
-                //     console.log(err);
-                // } else {
-                //     global.food_items = data;
-                // }
+                })
             });
+            // listCollections({name: 'food_items'}).toArray(function (err, database) {
+            // });
+            //     module.exports.Collection = database;
+            // });
         }
-    });
-}
-module.exports = mongoDB;
+    })
+};
