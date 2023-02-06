27f1cf34f4393dfe01c00c4af422bbeadde7f434
Keyur Raval <ravalkeyur054@gmail.com>
2023-02-06 18:47:09 +0530

    Style Updates

--- src/components/Carousal.js
+++ src/components/Carousal.js
@@ -1,24 +1,26 @@
 import React from 'react'
 
-const Carousal = () => {
+export default function Carousel() {
     return (
         <div>
-            <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel" style={{ objectFit: "contain !important" }}>
-                <div className="carousel-inner" id='crr'>
-                    <div className='carousel-caption' style={{ zIndex: "10" }}>
-                        <form className="d-flex">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success text-white bg-success" type="submit">Search</button>
+
+            <div id="carouselExampleFade" className="carousel slide carousel-fade " data-bs-ride="carousel">
+
+                <div className="carousel-inner " id='carousel'>
+                    <div class=" carousel-caption  " style={{ zIndex: "9" }}>
+                        <form className=" d-flex justify-content-center">  {/* justify-content-center, copy this <form> from navbar for search box */}
+                            <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Type in..." aria-label="Search" />
+                            <button className="btn text-white bg-success" type="submit">Search</button>
                         </form>
                     </div>
-                    <div className="carousel-item active">
-                        <img src="https://source.unsplash.com/random/400x100/?burger" className="d-block w-100" alt="..." />
+                    <div className="carousel-item active" >
+                        <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100  " style={{ filter: "brightness(30%)" }} alt="..." />
                     </div>
                     <div className="carousel-item">
-                        <img src="https://source.unsplash.com/random/400x100/?momos" className="d-block w-100" alt="..." />
+                        <img src="https://source.unsplash.com/random/900x700/?pastry" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
                     </div>
                     <div className="carousel-item">
-                        <img src="https://source.unsplash.com/random/400x100/?bacon" className="d-block w-100" alt="..." />
+                        <img src="https://source.unsplash.com/random/900x700/?barbeque" className="d-block w-100 " style={{ filter: "brightness(30%)" }} alt="..." />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
@@ -30,8 +32,8 @@ const Carousal = () => {
                     <span className="visually-hidden">Next</span>
                 </button>
             </div>
+
+
         </div>
     )
 }
-
-export default Carousal
