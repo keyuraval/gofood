7df92fc079f06f2d24651709faab57aab72bdf50
Keyur Raval <ravalkeyur054@gmail.com>
2023-02-06 18:48:08 +0530

    CRUD Context created

--- src/components/ContextReducer.js
+++ src/components/ContextReducer.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useReducer } from 'react'
+import React, { useReducer, useContext, createContext } from 'react';
 
 const CartStateContext = createContext();
 const CartDispatchContext = createContext();
@@ -6,26 +6,40 @@ const CartDispatchContext = createContext();
 const reducer = (state, action) => {
     switch (action.type) {
         case "ADD":
-            return [...state, { id: action.id, name: action.name, qty: action.qty, size: action.size, price: action.price, img: action.price }]
+            return [...state, { id: action.id, name: action.name, qty: action.qty, size: action.size, price: action.price, img: action.img }]
+        case "REMOVE":
+            let newArr = [...state]
+            newArr.splice(action.index, 1)
+            return newArr;
+        case "DROP":
+            let empArray = []
+            return empArray
+        case "UPDATE":
+            let arr = [...state]
+            arr.find((food, index) => {
+                if (food.id === action.id) {
+                    console.log(food.qty, parseInt(action.qty), action.price + food.price)
+                    arr[index] = { ...food, qty: parseInt(action.qty) + food.qty, price: action.price + food.price }
+                }
+                return arr
+            })
+            return arr
         default:
-            console.log("Error in reducer.")
+            console.log("Error in Reducer");
     }
-}
-
-
+};
 
 export const CartProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, []);
+
     return (
         <CartDispatchContext.Provider value={dispatch}>
             <CartStateContext.Provider value={state}>
                 {children}
             </CartStateContext.Provider>
         </CartDispatchContext.Provider>
-
     )
-}
+};
 
 export const useCart = () => useContext(CartStateContext);
-export const useDispathcCart = () => useContext(CartDispatchContext);
-
+export const useDispatchCart = () => useContext(CartDispatchContext);
