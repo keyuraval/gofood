02fbad46582fd4e8eb410287964393a29f7ddded
Keyur Raval <ravalkeyur054@gmail.com>
2023-02-06 18:54:39 +0530

    All ROutes Created

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-
 import './App.css';
-// import Navbar from './components/Navbar';
-// import Home from './screens/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './screens/Home';
-import Login from './screens/Login';
-import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'  //npm i bootstrap-dark-5 boostrap
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import Home from './screens/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+// import Navbar from './components/Navbar';
+import Login from './screens/Login';
 import Signup from './screens/Signup';
 import { CartProvider } from './components/ContextReducer';
+import MyOrder from './screens/MyOrder';
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <CartProvider>
-
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/signup" element={<Signup />} />
-        </Routes>
-
-      </CartProvider></BrowserRouter>
-
+    <CartProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/myorder" element={<MyOrder />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
 
