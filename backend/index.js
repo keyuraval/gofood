fdc0def50a354314323fec4b2c4b2fa349070fa6
Keyur Raval <ravalkeyur054@gmail.com>
2023-02-23 19:14:26 +0530

    CORS

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,13 @@ app.use((req, res, next) => {
     );
     next();
 });
-app.use(cors());
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 app.use(express.json())
 
 app.get('/', (req, res) => {
